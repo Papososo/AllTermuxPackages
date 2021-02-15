fb3c86b91a069a30edf2d5bc8dd3836598e3094f
PaposinDT <63978882+PaposinDT@users.noreply.github.com>
2021-02-15 15:18:57 +0100

    Add files via upload

---
+++ AllTermuxPackages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Version 1.1
+# coded By Riccardo Papa
+# copyright(please read the license before sharing the code)
+clear
+echo """
+		 ____________________________________________
+		|			                                 |
+		|        The Code By :- Riccardo Papa        |
+		|	     Telegram :- @PaposinDT              |
+		|        Facebook :- https://cutt.us/kfNBv   |
+		|        Youtube :-  https://cutt.us/fp49D   |
+		|        Git Hub :- github.com/PaposinDT     |
+        |        Instagram:- https://cutt.us/2ZDRu   |
+		|___________________________________________ | """
+sleep 3
+
+echo "                           	Version 1.1" 
+echo "                    	  Coded By Riccardo Papa"
+echo "            	  Want to support me?  paypal.me/ricky2006 "
+sleep 1
+echo "                         ENJOY YOUR TERMUX PACKAGES"
+sleep 1
+echo " Installing..."
+sleep 2
+pip3 install -r requirements.txt
+
